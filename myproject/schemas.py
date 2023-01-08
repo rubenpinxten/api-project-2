5e82cc9556e6fcacadd3c6199b42dffa00921574
Ruben Pinxten <91123119+rubenpinxten@users.noreply.github.com>
2023-01-09 00:04:50 +0100

    t

--- myproject/schemas.py
+++ myproject/schemas.py
@@ -1,35 +1,21 @@
 from pydantic import BaseModel
 
 
-class ItemBase(BaseModel):
-    title: str
-    description: str | None = None
-
-
-class ItemCreate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
-    id: int
-    owner_id: int
+class UserBase(BaseModel):
+    name: str
+    email: str
 
     class Config:
         orm_mode = True
 
+class ProductBase(BaseModel):
+    name: str
 
-class UserBase(BaseModel):
-    email: str
+    class Config:
+        orm_mode = True
 
-
-class UserCreate(UserBase):
-    password: str
-
-
-class User(UserBase):
-    id: int
-    is_active: bool
-    items: list[Item] = []
+class ManufactorBase(BaseModel):
+    name: str
 
     class Config:
         orm_mode = True
