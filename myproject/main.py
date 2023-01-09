6f936c60537b231d0fc8863cff40c5d158e781ba
Ruben Pinxten <91123119+rubenpinxten@users.noreply.github.com>
2023-01-09 18:49:16 +0100

    t

--- myproject/main.py
+++ myproject/main.py
@@ -1,5 +1,5 @@
 from fastapi import Depends, FastAPI, HTTPException
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 import os
