2dc397e7e3f90180f8b126513e5a9e33cfe4b7c9
Ruben Pinxten <91123119+rubenpinxten@users.noreply.github.com>
2023-01-09 19:48:30 +0100

    t

--- myproject/main.py
+++ myproject/main.py
@@ -53,7 +53,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db),token: str = Depends(oauth2_scheme)):
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
